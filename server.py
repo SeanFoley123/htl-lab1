d63cdd7712280cbcc035f22937cf4d0102dac155
SeanFoley123 <sean.foley@students.olin.edu>
2017-02-01 14:49:18 -0500

    Pushing to Heroku

--- server.py
+++ server.py
@@ -59,7 +59,6 @@ def redir():
 	return redirect("/ALL")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', debug=True, port=port)
 debug
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', debug=True, port=port)
